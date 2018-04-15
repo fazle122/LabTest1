71d426c58ebbd7e85149568d111327027e3f9db8
fazle122 <fazlerabby122@gmail.com>
2018-04-15 22:21:15 +0600

    controller with method

---
+++ LabTest1/Controllers/FoodController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LabTest1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabTest1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Food")]
+    public class FoodController : Controller
+    {
+        [HttpGet]
+        [Route("GetFood")]
+        public IActionResult GetFood()
+        {
+            List<Food> food = new List<Food>();
+            food.Add(new Food() { Id = Guid.NewGuid().ToString(), Name = "a", Discription = "greate" });
+            food.Add(new Food() { Id = Guid.NewGuid().ToString(), Name = "b", Discription = "good" });
+            food.Add(new Food() { Id = Guid.NewGuid().ToString(), Name = "c", Discription = "moderate" });
+            return this.Ok(food);
+        }
+
+        [HttpPost]
+        [Route("SaveFood")]
+        public IActionResult SaveFood([FromBody] Food food)
+        {
+            food.Id = Guid.NewGuid().ToString();
+            return this.Ok(food);
+        }
+
+        [HttpDelete]
+        [Route("DeleteFood")]
+        public IActionResult DeleteFood(string id)
+        {
+            return this.Ok(id);
+        }
+
+
+        [HttpPut]
+        [Route("EditFood")]
+        public IActionResult EditFood(string id, Food foo)
+        {
+            if (id != null)
+            {
+                foo.Name = "test";
+                foo.Discription = "test";
+            }
+            return this.Ok(foo);
+        }
+
+
+    }
+}
---
+++ LabTest1/Controllers/RestaurentController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LabTest1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabTest1.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Restaurent")]
+    public class RestaurentController : Controller
+    {
+
+        [HttpGet]
+        [Route("GetRestaurent")]
+        public IActionResult GetRestaurent()
+        {
+            List<Restaurent> res = new List<Restaurent>();
+            res.Add(new Restaurent() { Id = Guid.NewGuid().ToString(), Name = "Fokhruddin", Discription = "Biriany" });
+            res.Add(new Restaurent() { Id = Guid.NewGuid().ToString(), Name = "Chillox", Discription = "Barger" });
+            res.Add(new Restaurent() { Id = Guid.NewGuid().ToString(), Name = "Bonoful", Discription = "sweet" });
+            return this.Ok(res);
+        }
+
+        [HttpPost]
+        [Route("SaveRestaurent")]
+        public IActionResult SaveRestaurent([FromBody] Restaurent res)
+        {
+            res.Id = Guid.NewGuid().ToString();
+            return this.Ok(res);
+        }
+
+        [HttpDelete]
+        [Route("DeleteRestaurent")]
+        public IActionResult DeleteRestaurent(string id)
+        {
+            return this.Ok(id);
+        }
+
+        [HttpPut]
+        [Route("EditRestaurent")]
+        public IActionResult EditRestaurent(string id,Restaurent res)
+        {
+            if(id != null)
+            {
+                res.Name = "updateRes";
+                res.Discription = "updateTest";
+            }
+            return this.Ok(res);
+        }
+    }
+}
